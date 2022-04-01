7ff1c2b9074906f6eec8e88d93c10bf317b4a7b0
Mithilesh Tiwari <Mithilesh@crowdbotics.com>
2022-04-01 23:34:22 +0530

    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -35,6 +35,15 @@ const Untitled1 = () => {
       backgroundColor: "#E4E4E4",
       borderRadius: 0,
       color: "#777777"
+    }}></View><View style={{
+      left: 42,
+      top: 267,
+      position: "absolute",
+      height: 60,
+      width: 140,
+      backgroundColor: "#E4E4E4",
+      borderRadius: 0,
+      color: "#777777"
     }}></View></View>;
 };
 
