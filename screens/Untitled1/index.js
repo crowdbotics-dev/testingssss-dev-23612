80b906ed786d563f1b81b2292a73f5cc6db40904
Mithilesh Tiwari <Mithilesh@crowdbotics.com>
2022-04-04 13:59:29 +0530

    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -3,33 +3,14 @@ import { View } from "react-native";
 
 const Untitled1 = () => {
   return <View style={{
-    backgroundColor: "#f0f0f1",
+    backgroundColor: '#f0f0f1',
     padding: 10,
-    position: "relative",
-    height: "100%",
-    borderRadius: "50%"
+    position: 'relative',
+    height: '100%'
   }}>
       <View style={{
-      left: 40,
-      top: 82,
-      position: "absolute",
-      height: 60,
-      width: 140,
-      backgroundColor: "#E4E4E4",
-      borderRadius: 0,
-      color: "#777777"
-    }}></View><View style={{
-      left: 35,
-      top: 167,
-      position: "absolute",
-      height: 60,
-      width: 140,
-      backgroundColor: "#E4E4E4",
-      borderRadius: 0,
-      color: "#777777"
-    }}></View><View style={{
-      left: 42,
-      top: 267,
+      left: 39,
+      top: 79,
       position: "absolute",
       height: 60,
       width: 140,
