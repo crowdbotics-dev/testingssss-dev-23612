3a2627162862fc1cb0a9a081f4cdbe22ba969f7b
Mithilesh Tiwari <Mithilesh@crowdbotics.com>
2022-04-04 16:02:20 +0530

    Crowdbotics Studio: Update app screens

--- screens/Untitled2/index.js
+++ screens/Untitled2/index.js
@@ -8,66 +8,7 @@ const Untitled2 = () => {
     position: 'relative',
     height: '100%'
   }}>
-      <View style={{
-      left: 19,
-      top: 212,
-      position: "absolute",
-      height: 60,
-      width: 140,
-      backgroundColor: "#E4E4E4",
-      borderRadius: 0,
-      color: "#777777"
-    }}><View style={{
-        left: 10,
-        top: 54,
-        position: "absolute",
-        width: 80,
-        height: 80,
-        backgroundColor: "#E4E4E4",
-        borderRadius: "50%"
-      }}><View style={{
-          left: 15,
-          top: -90,
-          position: "absolute",
-          width: 80,
-          height: 80,
-          backgroundColor: "#E4E4E4",
-          borderRadius: "50%"
-        }}><View style={{
-            left: 63,
-            top: 0,
-            position: "absolute",
-            width: 80,
-            height: 80,
-            backgroundColor: "#E4E4E4",
-            borderRadius: "50%"
-          }}></View></View></View></View><View style={{
-      left: 25,
-      top: 249,
-      position: "absolute",
-      height: 60,
-      width: 140,
-      backgroundColor: "#E4E4E4",
-      borderRadius: 0,
-      color: "#777777"
-    }}></View><View style={{
-      left: 27,
-      top: 162,
-      position: "absolute",
-      height: 60,
-      width: 140,
-      backgroundColor: "#E4E4E4",
-      borderRadius: 0,
-      color: "#777777"
-    }}></View><View style={{
-      left: 75,
-      top: 155,
-      position: "absolute",
-      width: 80,
-      height: 80,
-      backgroundColor: "#E4E4E4",
-      borderRadius: "50%"
-    }}></View></View>;
+      </View>;
 };
 
 export default Untitled2;
