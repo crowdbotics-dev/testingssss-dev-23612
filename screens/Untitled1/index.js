bcb46c7eca32ebe03f56550cc15a09005aa91a56
Mithilesh Tiwari <Mithilesh@crowdbotics.com>
2022-07-22 14:42:29 +0530

    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -12,8 +12,8 @@ const Untitled1 = () => {
       left: 14,
       top: 90,
       position: "absolute",
-      height: 60,
-      width: 140,
+      height: 90,
+      width: 90,
       backgroundColor: "#E4E4E4",
       borderRadius: 0,
       color: "#777777"
